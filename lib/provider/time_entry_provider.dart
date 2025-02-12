1ec2e5faa24f3069ebec1ce70c35247b16c9687b
Mohsin Ali Rashid <mohsinalikhawaja1111@gmail.com>
2025-02-12 18:13:16 +0100

    Update time_entry_provider.dart

--- lib/provider/time_entry_provider.dart
+++ lib/provider/time_entry_provider.dart
@@ -6,9 +6,13 @@ import 'package:time_tracking_app/models/time_entry.dart';
 class TimeEntryProvider with ChangeNotifier {
   final LocalStorage storage;
   List<TimeEntry> _entries = [];
+  final List<String> _projects = []; // Store user-added projects
+  final List<String> _tasks = []; // Store user-added tasks
 
   // Getters
   List<TimeEntry> get entries => _entries;
+  List<String> get projects => _projects;
+  List<String> get tasks => _tasks;
 
   // Load entries from Storage
   TimeEntryProvider(this.storage) {
@@ -19,7 +23,7 @@ class TimeEntryProvider with ChangeNotifier {
     var storedEntries = storage.getItem('entries');
     if (storedEntries != null) {
       _entries = List<TimeEntry>.from(
-        (storedEntries as List).map((item) => Project.fromJson(item)),
+        (storedEntries as List).map((item) => TimeEntry.fromJson(item)),
       );
       notifyListeners();
     }
@@ -34,10 +38,4 @@ class TimeEntryProvider with ChangeNotifier {
     _entries.removeWhere((entry) => entry.id == id);
     notifyListeners();
   }
-
-  // void _saveTimeEntryToStorage() {
-  //   storage.setItem(
-  //       'entries', jsonEncode(_entries.map((e) => e.toJson()).toList()));
-  // }
-  // Add a Project
 }
